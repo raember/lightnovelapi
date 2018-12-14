f25daa47a234423a47b1fedbe1392d8c83289a4b
raember <raphael.emberger@hotmail.ch>
2018-12-14 23:29:01 +0900

    Write test and implementation for chapter parsing

--- lightnovel/wuxiaworld/api.py
+++ lightnovel/wuxiaworld/api.py
@@ -1,13 +1,21 @@
 from lightnovel.api import LightNovelApi
 from bs4 import BeautifulSoup
 from .novel import WuxiaWorldNovel
+from .chapter import WuxiaWorldChapter
 
 
 class WuxiaWorld(LightNovelApi):
 
     def get_novel(self, novel_path: str) -> WuxiaWorldNovel:
-        url = "https://www.wuxiaworld.com/novel/{}".format(novel_path.lower())
+        url = "https://www.wuxiaworld.com/novel/{}".format(novel_path)
         response = self._request('GET', url)
         response.raise_for_status()
         bs = BeautifulSoup(response.text, features="html5lib")
         return WuxiaWorldNovel(bs)
+
+    def get_chapter(self, chapter_path: str) -> WuxiaWorldChapter:
+        url = "https://www.wuxiaworld.com/novel/{}".format(chapter_path)
+        response = self._request('GET', url)
+        response.raise_for_status()
+        bs = BeautifulSoup(response.text, features="html5lib")
+        return WuxiaWorldChapter(bs)
