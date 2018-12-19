bf5eb406ef541ef51fd9147cf1465c81901308af
raember <raphael.emberger@hotmail.ch>
2018-12-19 16:55:15 +0900

    Wrote test for second chapter page of HJC and for Latex sink.

--- lightnovel/wuxiaworld/api.py
+++ lightnovel/wuxiaworld/api.py
@@ -7,14 +7,14 @@ from .chapter import WuxiaWorldChapter
 class WuxiaWorld(LightNovelApi):
 
     def get_novel(self, novel_path: str) -> WuxiaWorldNovel:
-        url = "https://www.wuxiaworld.com/novel/{}".format(novel_path)
+        url = "https://www.wuxiaworld.com{}".format(novel_path)
         response = self._request('GET', url)
         response.raise_for_status()
         bs = BeautifulSoup(response.text, features="html5lib")
         return WuxiaWorldNovel(bs)
 
     def get_chapter(self, chapter_path: str) -> WuxiaWorldChapter:
-        url = "https://www.wuxiaworld.com/novel/{}".format(chapter_path)
+        url = "https://www.wuxiaworld.com{}".format(chapter_path)
         response = self._request('GET', url)
         response.raise_for_status()
         bs = BeautifulSoup(response.text, features="html5lib")
