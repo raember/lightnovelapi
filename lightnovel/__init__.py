680713c71ce89e71884519f0d890f5900c30bdad
raember <raphael.emberger@hotmail.ch>
2018-12-21 18:23:33 +0900

    Scraped SFF LN from WW and wrote tests. Optimized data cleaner to have smaller files. Reconstructed mock data files with said data cleaner. Made dev branch. Solved recursive import statement problem.

--- lightnovel/__init__.py
+++ lightnovel/__init__.py
@@ -1,4 +1,4 @@
-from .api import LightNovelApi, Novel, Book, ChapterEntry, Chapter, request, LogBase
+from .api import LightNovelApi, Novel, Book, ChapterEntry, Chapter, LogBase, request
 from .wuxiaworld import WuxiaWorldApi
 
 __version__ = "0.1"
