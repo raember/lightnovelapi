cddb257c96917061e373fbd144b0662cf70ec0b5
raember <raphael.emberger@hotmail.ch>
2019-01-16 17:24:29 +0900

    Split up utilities and wrote tests. How tf do I solve those circular imports?

--- lightnovel/__init__.py
+++ lightnovel/__init__.py
@@ -1,11 +1,4 @@
-from .api import LightNovelApi, Novel, Book, ChapterEntry, Chapter, request
+from .api import LightNovelApi, Novel, Book, ChapterEntry, Chapter
 from .wuxiaworld import WuxiaWorldApi
-import logging
-
-logging.basicConfig(
-        format='%(asctime)s %(levelname)-8s %(name)16s: %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
-        level=logging.DEBUG
-)
 
 __version__ = "0.1"
