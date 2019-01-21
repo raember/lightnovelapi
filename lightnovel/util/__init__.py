8d9c3ca5b5064480b8d2067b756748452d12e87e
raember <raphael.emberger@hotmail.ch>
2019-01-21 18:05:27 +0900

    Expanded the proxy classes. Fixed missing mimetype in datasets. Changed api from accepting paths to only full urls. Privatized sink functions. Mended code and corresponding tests.

--- lightnovel/util/__init__.py
+++ lightnovel/util/__init__.py
@@ -1,4 +1,4 @@
 from .text import slugify
 from .sink import HtmlSink, StringHtmlSink, MarkdownHtmlSink, LatexHtmlSink
-from .proxy import HarProxy, HtmlProxy, request, ipinfo
+from .proxy import HarProxy, HtmlProxy, DirectProxy, PersistToHtmlProxy, ipinfo
 # from .factory import get_api
