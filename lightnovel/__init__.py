b19786e05fb0fcbc03f89eafd780f670a01bd27b
emberrap <emberrap@students.zhaw.ch>
2019-06-12 23:03:38 +0200

    Introduced generator based pipelines Fixed tests

--- lightnovel/__init__.py
+++ lightnovel/__init__.py
@@ -1,6 +1,8 @@
 # noinspection PyUnresolvedReferences
 from .api import LightNovelApi, Novel, Book, ChapterEntry, Chapter, SearchEntry
 # noinspection PyUnresolvedReferences
+from .pipeline import Pipeline, ChapterConflation
+# noinspection PyUnresolvedReferences
 from .transform import Transformer, EpubTransformer
 
 __version__ = "0.1"
