9247c1c30367685c667393c920c63454bf926464
raember <raember@users.noreply.github.com>
2021-02-20 00:48:15 +0100

    Improved main script

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
 from typing import List
 
 from spoofbot import Firefox, Chrome, Windows, MacOSX, Linux, OS, Browser
@@ -45,101 +46,143 @@ def get_log(verbose: bool) -> logging.Logger:
     return logger
 
 
-# noinspection SpellCheckingInspection
-log_format = '%(asctime)s %(levelname)-8s %(name)23s: %(message)s'
-date_format = '%Y-%m-%d %H:%M:%S'
+def search_apis(apis: List[LightNovelApi], search: str = None) -> Url:
+    search_results: List[NovelEntry] = []
+    for api in apis:
+        if isinstance(api, WebNovelComApi):
+            matches = api.search_for_specific_title(args.search)
+        else:
+            matches = api.search(search)
+        for search_result in matches:
+            log.info(f"{len(search_results)} - {search_result} ({search_result.url})")
+            search_results.append(search_result)
+    if len(search_results) == 0:
+        log.error("Could not find any matching title.")
+        exit(1)
+    else:
+        choice = None
+        if len(search_results) == 1:
+            choice = search_results[0]
+        while choice is None:
+            try:
+                choice = search_results[int(input('Please choose the index of the url to scrape: '))]
+            except KeyboardInterrupt:
+                log.info("Stopping")
+                exit(1)
+            except Exception as e:
+                log.error(f"Error while reading input: {e}")
+        return choice.url
 
-if __name__ == '__main__':
+
+def get_args() -> Namespace:
     parser = ArgumentParser(description='Lightnovel API client.')
-    parser.add_argument(dest="urls", action="store", metavar='URL', nargs='*', type=parse_url, default=[],
-                        help='urls of the web novels to scrape')
+    # Setup
     parser.add_argument('-b', '--browser', dest='browser', action='store', metavar='NAME',
                         choices=['ff', 'chrome'], default='ff',
                         help='the browser to use (ff/chrome). Default: ff')
     parser.add_argument('-o', '--os', dest='os', action='store', metavar='OS',
                         choices=['win', 'mac', 'linux'], default='win',
                         help='the OS to use (win/mac/linux). Default: win')
+    # Cache
     parser.add_argument('-c', '--cache', dest='cache', action='store', default='.cache',
                         help='the cache folder. set to empty string for no cache.')
-    parser.add_argument('--search', nargs='?', const='?',
-                        help='a title to search for on available hosters')
-    parser.add_argument('--use-cached', dest='use_cached', action='store_true', default=False,
-                        help='use cached novel urls')
+
+    # Jobs
     parser.add_argument('--update', dest='update', action='store_true', default=False,
                         help='only check for updates chapters')
     parser.add_argument('--epub', dest='epub', action='store_true', default=False,
                         help='compile epub files from the books')
+
+    # Novel set
+    parser.add_argument(dest="urls", action="store", metavar='URL', nargs='*', type=parse_url, default=[],
+                        help='urls of the web novels to scrape')
+    parser.add_argument('--search', nargs='?', const='?',
+                        help='a title to search for on available hosters. Leave empty to search interactively')
+    parser.add_argument('--use-cached', dest='use_cached', action='store_true', default=False,
+                        help='use cached novel urls')
+
+    # Apis
     parser.add_argument('--wuxiaworld', dest='ww', action='store_true', default=False,
-                        help='Iterate through all hosted WuxiaWorld books')
+                        help='Process all hosted WuxiaWorld books')
     parser.add_argument('--qidian-underground', dest='qu', action='store_true', default=False,
-                        help='Iterate through all hosted QidianUnderground books (takes webnovel into account)')
+                        help='Process all hosted QidianUnderground books (takes webnovel into account)')
+
+    # Miscellaneous
     parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', default=False,
                         help='be verbose')
     # noinspection SpellCheckingInspection
     parser.add_argument('--version', action='version', version=f'%(prog)s {lightnovel.__version__}')
-    args = parser.parse_args()
 
-    log = get_log(args.verbose)
+    return parser.parse_args()
+
+
+# noinspection SpellCheckingInspection
+log_format = '%(asctime)s %(levelname)-8s %(name)23s: %(message)s'
+date_format = '%Y-%m-%d %H:%M:%S'
+
+if __name__ == '__main__':
+    args = get_args()
+
+    # Setup
     browser = get_browser(args.browser, os=get_os(args.os))
-    adapter = FileCacheAdapter(args.cache)
-    if args.cache != '':
-        browser.adapter = adapter
+    log = get_log(args.verbose)
+
+    # Cache
+    use_cache = args.cache != ''
+    if use_cache:
+        cache = Path(args.cache)
+        log.info(f"Using cache dir: {cache}")
+        cache.parent.mkdir(parents=True, exist_ok=True)
+        adapter = FileCacheAdapter(str(cache.absolute()))
     else:
         log.warning("Not using a cache.")
         adapter = browser.get_adapter('https://')
+    browser.adapter = adapter
 
-    if args.update and args.cache == '':
+    # Job
+    if args.update and not use_cache:
         log.error("Cannot only check for updated chapters without a cache.")
         exit(1)
     if args.epub and args.update:
         log.error("Cannot only check for updated chapters but still compile a full epub.")
         exit(1)
+    if args.update:
+        log.info("Job: Update novel(s)")
+    elif args.epub:
+        log.info("Job: Compile epub for novel(s)")
+    else:
+        log.info("Job: Scrape full novel(s)")
 
-    urls: List[Url] = args.urls
-    # noinspection PyTypeChecker
+    # Novel set
+    urls: List[Url] = []
     ww_api = WuxiaWorldComApi(browser)
     wn_api = WebNovelComApi(browser)
     qu_api = QidianUndergroundOrgApi(browser)
     wn_api.qidian_underground_api = qu_api
     apis = [ww_api, wn_api]
-    if args.search is not None:
+    if args.urls:
+        log.info(f"Processing {len(args.urls)} novel{'s' if len(args.urls) >= 0 else ''}")
+        urls = list(map(parse_url, args.urls))
+    # noinspection PyTypeChecker
+    elif args.search is not None:
         if args.search == '?':
             args.search = input("Please enter a title to search for: ")
-        search_results: List[NovelEntry] = []
-        for api in [ww_api, wn_api]:
-            if isinstance(api, WebNovelComApi):
-                matches = api.search_for_specific_title(args.search)
-            else:
-                matches = api.search(args.search)
-            for search_result in matches:
-                log.info(f"{len(search_results)} - {search_result} ({search_result.url})")
-                search_results.append(search_result)
-        if len(search_results) == 0:
-            log.error("Could not find any matching title.")
-        else:
-            choice = None
-            if len(search_results) == 1:
-                choice = search_results[0]
-            while choice is None:
-                try:
-                    choice = search_results[int(input('Please choose the index of the url to scrape: '))]
-                except KeyboardInterrupt:
-                    log.info("Stopping")
-                    exit(1)
-                except Exception as e:
-                    log.error(f"Error while reading input: {e}")
-            urls.append(choice.url)
-
-    missed_novels = []
-
-    if args.ww:
-        for entry in ww_api.search(count=100):
+        urls.append(search_apis(apis, args.search))
+    elif args.ww:
+        log.info("Scraping entire WuxiaWorld")
+        entries = []
+        for entry in ww_api.search(count=200):
             if entry.url.path.startswith('/novel'):
+                log.info(f"  - {entry.title}")
+                entries.append(entry)
                 urls.append(entry.url)
             else:
                 log.debug(f"Discarding {entry}, because it's a preview novel.")
-
-    if args.qu:
+        log.info(f"Found {len(entries)} novels")
+    elif args.qu:
+        log.info("Scraping entire Webnovel/QidianUnderground")
+        entries = []
+        missed_novels = []
         for entry in qu_api.search():
             matches = wn_api.search_for_specific_title(entry.title)
             if len(matches) == 0:
@@ -151,20 +194,21 @@ if __name__ == '__main__':
                     log.info(f"Chose {first_match} out of: {', '.join(map(str, matches))}")
                 urls.append(first_match.url)
         log.info(f"Found {len(urls)}/{len(urls) + len(missed_novels)} novels ({len(missed_novels)} missed)")
-
-    if args.use_cached:
+    elif args.use_cached:
         if not isinstance(adapter, FileCacheAdapter):
             log.error("Cannot use cached urls without a cache.")
             exit(1)
-        for api in [ww_api, wn_api]:
+        log.info("Only processing already cached novels:")
+        for api in apis:
             cached = adapter.list_cached(api.novel_url)
-            log.info(f"{api.hoster}: {len(cached)} cached novels")
+            log.info(f"  {api.hoster}: {len(cached)} cached novels")
             urls += cached
-
-    if len(urls) == 0 or not args.epub and not args.update:
+    else:
         log.info("Nothing to do")
         exit(0)
 
+    # Start
+    input("Hit [Enter] to start: ")
     stats_maker = StatisticsMaker(browser)
     try:
         for url in urls:
